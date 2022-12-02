3dc705eaef57fed7522ff5d91efff68c06425f18
T.C <6162887+taocao@users.noreply.github.com>
2022-12-02 21:19:58 +0000

    Create test_pipeline.py

---
+++ test_pipeline.py
@@ -0,0 +1,23 @@
+import pytest
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_pipeline():
+    # Load the iris dataset
+    X, y = load_iris(return_X_y=True)
+
+    # Create a pipeline with a logistic regression model
+    model = Pipeline([
+        ('lr', LogisticRegression())
+    ])
+
+    # Fit the model on the training data
+    model.fit(X, y)
+
+    # Use the model to make predictions on the test data
+    y_pred = model.predict(X)
+
+    # Assert that the model has a high accuracy
+    assert accuracy_score(y, y_pred) >= 0.9
